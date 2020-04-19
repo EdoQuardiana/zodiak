d50113eb03a2c4baa0c8be520a02062bf7f6f936
edo <edo@gmail.com>
2020-04-18 21:26:47 -0800

    Initial commit

---
+++ lib/utils/strings.dart
@@ -0,0 +1,224 @@
+class Strings {
+  static const String APP_NAME = "Zodiac Signs";
+
+  static const List<String> SIGN_NAME = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces"
+  ];
+
+  static const List<String> SIGN_DATES = [
+    "March 21 - April 20",
+    "April 21 - May 21",
+    "May 22 - June 22",
+    "June 23 - July 22",
+    "July 23 - August 22",
+    "August 23 - September 22",
+    "September 23 - Oktober 22",
+    "Oktober 23 - November 21",
+    "November 22 - December 21",
+    "December 22 - January 21",
+    "January 22 - February 19",
+    "February 20 - March 20"
+  ];
+
+  static const List<String> SIGN_GENERAL_DETAILS =[
+        "\tRuling Planet: Mars\n\n"
+        "\tElement: Fire\n\n"
+        "\tLucky Day: Tuesday\n\n"
+        "\tLucky Number: 8\n\n"
+        "\tLucky Color: Red\n\n"
+        "\tLucky Stone: Opal\n\n"
+        "\tLucky Flower: Tulip\n\n"
+        "\tLucky Scent: Magnolia\n\n"
+        "\tStrengths: Courage\n\n"
+        "\tWeaknesses: Hasty\n\n"
+        "\tGreatest Overall Compatibility: Leo, Sagittarius\n\n"
+        "\tWorst Overall Compatibility: Scorpio, Libra, Capricorn\n\n"
+        "\tBest Job: Politician, Engineer, Painter, Musician, Acting\n\n"
+        "\tFamous People: Descartes, Charlie Chaplin, Vincent Van Gogh\n\n",
+
+        "\tRuling Planet: Venus\n\n"
+        "\tElement: Earth\n\n"
+        "\tLucky Day: Friday\n\n"
+        "\tLucky Number: 6\n\n"
+        "\tLucky Color: Blue\n\n"
+        "\tLucky Stone: Krizokol\n\n"
+        "\tLucky Flower: Red Rose\n\n"
+        "\tLucky Scent: Clove\n\n"
+        "\tStrengths: Reliable\n\n"
+        "\tWeaknesses: Stubborn\n\n"
+        "\tGreatest Overall Compatibility: Scorpio, Pisces\n\n"
+        "\tWorst Overall Compatibility: Aquarius, Leo, Cancer\n\n"
+        "\tBest Job: Askerlik, Doctor, Architech, Engineer, Banking\n\n"
+        "\tFamous People: Salvadore Dali, Karl Marks, Lenin, Sigmund Freud, Shakespare\n\n",
+
+
+        "\tRuling Planet: Mercury\n\n"
+        "\tElement: Air\n\n"
+        "\tLucky Day: Wednesday\n\n"
+        "\tLucky Number: 5\n\n"
+        "\tLucky Color: Yellow\n\n"
+        "\tLucky Stone: Quarts\n\n"
+        "\tLucky Flower: Mimosa\n\n"
+        "\tLucky Scent: Jasmine\n\n"
+        "\tStrengths: Talky\n\n"
+        "\tWeaknesses: Unstable\n\n"
+        "\tGreatest Overall Compatibility: Libra, Aquarius, Leo\n\n"
+        "\tWorst Overall Compatibility: Virgo, Sagittarius\n\n"
+        "\tBest Job: Writing, Banking, Advocacy, Journalism\n\n"
+        "\tFamous People: John F. Kennedy, John Wayne, Schumann\n\n",
+
+
+
+        "\tRuling Planet: Moon\n\n"
+        "\tElement: Water\n\n"
+        "\tLucky Day: Monday\n\n"
+        "\tLucky Number: 2\n\n"
+        "\tLucky Color: White\n\n"
+        "\tLucky Stone: Ametist\n\n"
+        "\tLucky Flower: White Rose\n\n"
+        "\tLucky Scent: Musk\n\n"
+        "\tStrengths: Patient\n\n"
+        "\tWeaknesses: Carelessness\n\n"
+        "\tGreatest Overall Compatibility: Taurus, Pisces\n\n"
+        "\tWorst Overall Compatibility: Aries, Leo, Aquarius\n\n"
+        "\tBest Job: Pharmacist, Pilot, Chemist\n\n"
+        "\tFamous People: Louis Armstrong, Tom Cruise, Giorgio Armani, Lady Diana\n\n",
+
+        "\tRuling Planet: Sun\n\n"
+        "\tElement: Fire\n\n"
+        "\tLucky Day: Monday\n\n"
+        "\tLucky Number: 1\n\n"
+        "\tLucky Color: Yellow\n\n"
+        "\tLucky Stone: Sardoniks, Crizolit\n\n"
+        "\tLucky Flower: Red Rose\n\n"
+        "\tLucky Scent: Musk\n\n"
+        "\tStrengths: Generosity\n\n"
+        "\tWeaknesses: Smug\n\n"
+        "\tGreatest Overall Compatibility: Aries, Sagittarius\n\n"
+        "\tWorst Overall Compatibility: Taurus, Cancer, Aquarius\n\n"
+        "\tBest Job: Soldier, Doctor\n\n"
+        "\tFamous People: Neil Armstrong, Benito Mussolini, Fidel Castro, Henry Ford, Madonna\n\n",
+
+        "\tRuling Planet: Mercury\n\n"
+        "\tElement: Earth\n\n"
+        "\tLucky Day: Wednesday\n\n"
+        "\tLucky Number: 5\n\n"
+        "\tLucky Color: Cobalt\n\n"
+        "\tLucky Stone: Ametist\n\n"
+        "\tLucky Flower: Yellow Violet\n\n"
+        "\tLucky Scent: Lemon\n\n"
+        "\tStrengths: Rigor\n\n"
+        "\tWeaknesses: Shy\n\n"
+        "\tGreatest Overall Compatibility: Taurus, Capricorn\n\n"
+        "\tWorst Overall Compatibility: Gemini, Sagittarius, Pisces\n\n"
+        "\tBest Job: Writing, Pharmacist\n\n"
+        "\tFamous People: Sophia Loren, Michael Jackson, Vitali Hakko, Richard Gere, Sean Connery\n\n",
+
+        "\tRuling Planet: Venus\n\n"
+        "\tElement: Air\n\n"
+        "\tLucky Day: Friday\n\n"
+        "\tLucky Number: 6\n\n"
+        "\tLucky Color: Turquoise\n\n"
+        "\tLucky Stone: Lapis\n\n"
+        "\tLucky Flower: Pink Rose\n\n"
+        "\tLucky Scents: Jasmine\n\n"
+        "\tStrengths: Elegant\n\n"
+        "\tWeaknesses: Pretentious\n\n"
+        "\tGreatest Overall Compatibility: Gemini, Aquarius\n\n"
+        "\tWorst Overall Compatibility: Aries, Scorpio, Capricorn\n\n"
+        "\tBest Job: Musician, Painter, Architech, Banking, Politician\n\n"
+        "\tFamous People: Cervantes, Oscar Wilde, Pavorotti, Pavorotti, Jimmy Carter\n\n",
+
+        "\tRuling Planet: Mars-Pluto\n\n"
+        "\tElement: Water\n\n"
+        "\tLucky Day: Tuesday\n\n"
+        "\tLucky Number: 9\n\n"
+        "\tLucky Color: Red and Black\n\n"
+        "\tLucky Stones: Akuamarin\n\n"
+        "\tUğurlu çiçekleriniz: Red Clove\n\n"
+        "\tLucky Scents: Musk\n\n"
+        "\tStrengths: Stable\n\n"
+        "\tWeaknesses: Ruthless\n\n"
+        "\tGreatest Overall Compatibility: Scorpio, Pisces\n\n"
+        "\tWorst Overall Compatibility: Taurus, Leo, Aquarius\n\n"
+        "\tBest Job: Engineer, Soldier, Writing, Musician, Journalism\n\n"
+        "\tFamous People: Maradona, Pablo Picasso, Prenses Charles, Indra Gandhi, Alfred Nobel\n\n",
+
+        "\tRuling Planet: Jupiter\n\n"
+        "\tElement: Fire\n\n"
+        "\tLucky Day: Thursday\n\n"
+        "\tLucky Number: 3\n\n"
+        "\tLucky Color: Purple\n\n"
+        "\tLucky Stones: Obsidyen\n\n"
+        "\tLucky Flowers: Lilac\n\n"
+        "\tLucky Scents: Lily\n\n"
+        "\tStrengths: Enterprising\n\n"
+        "\tWeaknesses: Snooty\n\n"
+        "\tGreatest Overall Compatibility: Aries, Leo\n\n"
+        "\tWorst Overall Compatibility: Gemini, Virgo, Pisces\n\n"
+        "\tBest Job: Politician, Advocate, Banking, Teacher\n\n"
+        "\tFamous People: Walt Disney, Winston Churchill, Ludwig van Beethoven, Mark Twain\n\n",
+
+
+        "\tRuling Planet: Saturn\n\n"
+        "\tElement: Earth\n\n"
+        "\tLucky Day: Saturday\n\n"
+        "\tLucky Number: 8\n\n"
+        "\tLucky Color: Grey\n\n"
+        "\tLucky Stone: Quartz\n\n"
+        "\tLucky Flower: Black Rose\n\n"
+        "\tLucky Scent: Camellia\n\n"
+        "\tStrengths: Stable\n\n"
+        "\tWeaknesses: Fanatical\n\n"
+        "\tGreatest Overall Compatibility: Taurus, Virgo\n\n"
+        "\tWorst Overall Compatibility: Aries, Scorpio, Libra\n\n"
+        "\tBest Job: Architech, Miner, Doctor\n\n"
+        "\tFamous People: Benjamin Franklin, Elvis Presley, Isaac Newton\n\n",
+
+
+
+        "\tRuling Planet: Uranus\n\n"
+        "\tElement: Air\n\n"
+        "\tLucky Day: Sunday\n\n"
+        "\tLucky Number: 4\n\n"
+        "\tLucky Color: Dark Blue\n\n"
+        "\tLucky Stone: Silver\n\n"
+        "\tLucky Flower: Violet\n\n"
+        "\tLucky Scent: Jasmine\n\n"
+        "\tStrengths: Willed\n\n"
+        "\tWeaknesses: Negligent\n\n"
+        "\tGreatest Overall Compatibility: Gemini, Libra\n\n"
+        "\tWorst Overall Compatibility: Taurus, Leo, Cancer\n\n"
+        "\tBest Job: Writing, Editor, Journalism, Artist\n\n"
+        "\tFamous People: Mozart, Ronald Reagan, Schubert, Abraham Lincoln, Kim Novak\n\n",
+
+
+
+
+        "\tRuling Planet: Neptun\n\n"
+        "\tElement: Water\n\n"
+        "\tLucky Day: Thursday\n\n"
+        "\tLucky Number: 7\n\n"
+        "\tLucky Color: Green\n\n"
+        "\tLucky Stone: Pink Quartz\n\n"
+        "\tLucky Flower: Lily\n\n"
+        "\tLucky Scents: Lemon\n\n"
+        "\tStrengths: Merciful\n\n"
+        "\tWeaknesses: Self-deprecating\n\n"
+        "\tGreatest Overall Compatibility: Scorpio, Cancer\n\n"
+        "\tWorst Overall Compatibility: Gemini, Virgo, Sagittarius\n\n"
+        "\tBest Job: Teacher, Musician, Writing, Doctor\n\n"
+        "\tFamous People: Yuri Gagarin, Albert Einstein, Gorbachev\n\n",
+  ];
+}
